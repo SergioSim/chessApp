8592f8ab9eac14ed1c4e3e82025f961a6d36c4ae
SergioSim <sergio.simonian@etu.unice.fr>
2018-12-01 17:27:29 +0100

    working on window size on resize

--- src/magicTools/Board.js
+++ src/magicTools/Board.js
@@ -2,32 +2,59 @@ import React, { Component } from 'react';
 import {Square} from './Square';
 
 export class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: 0,
+            height: 0
+        };
+    }
+
     renderSquare(i) {
       return <Square value={i}/>;
     }
+
+    updateDimentions(){
+        this.setState({width: window.innerWidth, height: window.innerHeight});
+        console.log("updating window size...");
+    }
+
+    componentWillMount(){
+        this.updateDimentions();
+    }
+
+    componentDidMount(){
+        window.addEventListener("resize", this.updateDimentions.bind(this));
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener("resize", this.updateDimentions.bind(this));
+    }
   
     render() {
-      const status = 'Next player: X';
+        const divStyle = {
+            height: this.state.height,
+            width: this.state.width
+        };
   
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
+        return (
+            <div style={divStyle}>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+            </div>
+        );
     }
   }
