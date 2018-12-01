b632263e9ac0421c573a861d1250131d8540efab
SergioSim <sergio.simonian@etu.unice.fr>
2018-12-01 11:56:35 +0100

    adding Board Game and Square

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Game} from './magicTools/Game';
 
 class App extends Component {
   render() {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Game />
         </header>
       </div>
     );
